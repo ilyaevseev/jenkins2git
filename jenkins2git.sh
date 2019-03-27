6fe173e81b3f1037ab154b486ed92efa1bab030f
Ilya Evseev <ilya.evseev@gmail.com>
2019-03-27 19:33:26 +0300

    jenkins2git: dont track fingerprints

--- jenkins2git.sh
+++ jenkins2git.sh
@@ -3,7 +3,7 @@
 cd "$JENKINS_HOME"
 
 # Add general configurations, secrets, job configurations, nodes, user content, users and plugins info:
-ls -1d *.xml secrets/ fingerprints/ jobs/*/*.xml nodes/*/*.xml userContent/* users/*/config.xml \
+ls -1d *.xml secrets/ jobs/*/*.xml nodes/*/*.xml userContent/* users/*/config.xml \
     plugins/*/META-INF/MANIFEST.MF 2>/dev/null | grep -v '^queue.xml$' | xargs -r -d '\n' git add --
 
 # Track deleted files:
