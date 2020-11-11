8abf06a630925da86197c9bfe30c6cd4e77b8abc
Ilya Evseev <ilya.evseev@gmail.com>
2020-11-11 09:01:15 +0300

    handle quoted names, handle "nothing added."

--- jenkins2git.sh
+++ jenkins2git.sh
@@ -7,9 +7,9 @@ ls -1d *.xml secrets/ jobs/*/*.xml nodes/*/*.xml userContent/* users/*/config.xm
     plugins/*/META-INF/MANIFEST.MF 2>/dev/null | grep -v '^queue.xml$' | xargs -r -d '\n' git add --
 
 # Track deleted files:
-LANG=C git status --porcelain | awk '$1 == "D" { print $2; }' | xargs -r git rm --ignore-unmatch
+LANG=C git status --porcelain | awk '$1 == "D" { $1=""; print }' | xargs -r git rm --ignore-unmatch
 
-LANG=C git status | grep -q '^nothing to commit' || {
+LANG=C git status | egrep -q '^nothing .*to commit' || {
     git commit -m "Automated Jenkins commit at $(date '+%Y-%m-%d %H:%M')"
     git push -q -u origin master
 }
